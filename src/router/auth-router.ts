0ab0ad5a343774a625d9fa74fe20d4a1aad8288d
MariyaN23 <sellaite505@gmail.com>
2024-09-30 15:11:24 +0300

    cookie

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -61,7 +61,7 @@ authRouter.get('/me',
 
 authRouter.delete('/logout', async (req: Request, res: Response) => {
     try {
-        res.clearCookie('token', { httpOnly: true, secure: true, path: "/" })
+        res.clearCookie('token', { httpOnly: false, secure: true, path: "/", sameSite: "none" })
         res.status(200).send({ message: 'Logout successfully' })
     } catch (error) {
         res.status(400).send({ message: `Error: ${error}` })
