947e9aafbec8a58db816607cd14a770f82657ff6
MariyaN23 <sellaite505@gmail.com>
2024-09-30 16:12:11 +0300

    cookie

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -38,7 +38,7 @@ authRouter.post('/login',
         try {
             const result = await authService.login(req.body.username, req.body.password);
             if (typeof result === 'object' && 'userId' in result) {
-                res.cookie("token", result.token, { httpOnly: false, secure: true,  path: "/", sameSite: "none" })
+                res.cookie("token", result.token, { httpOnly: false, secure: true,  path: "/",  maxAge:60*60*3*100000, sameSite: "none" })
                 res.status(200).json({userId: result.userId, token: result.token})
             } else {
                 res.status(400).send({ message: `Login error ${result}` });
